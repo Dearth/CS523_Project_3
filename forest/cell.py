d34032d61b4c63f071171c46ebedba9459273d92
Alex Johnson <alex_johnson3@yahoo.com>
2017-03-30 12:10:39 -0600

    Implement neighbor for cells in the grid.
    Extend list for simplicity, although we could just as easily index into a member variable.
    
    We store the cells in a linear array and have conversion methods for index_2d to get the row and col, and index_linear to get the corresponding cell for that row and col.

--- forest/cell.py
+++ forest/cell.py
@@ -1,25 +1,42 @@
 from enum import Enum
 
 
-class Forest:
-    # [], index
+class Forest(list):
     def __init__(self, rows=50, cols=50):
-        self.cells = [Cell() for _ in range(rows * cols)]
+        super().__init__(Cell(i) for i in range(rows * cols))
+        self.rows = rows
+        self.cols = cols
+
+    def index_2d(self, i):
+        return i // self.cols, i % self.cols
+
+    def index_linear(self, r, c):
+        return self[r * self.cols + c]
+
+    def neighbors(self, cell):
+        r, c = self.index_2d(cell.index)
+        return [self.index_linear(r + dr, c + dc)
+                for dr in range(-1, 2, 1) for dc in range(-1, 2, 1)
+                if (dr != 0 or dc != 0) and
+                0 <= r + dr < self.rows and
+                0 <= c + dc < self.cols]
 
     def step(self):
-        filter(Cell.on_fire, self.cells)
+        # TODO: the filter was originally taking into account the new_cell.catch_fire() calls
+        for fire in list(filter(Cell.on_fire, self)):
+            for new_cell in self.neighbors(fire):
+                new_cell.catch_fire()
 
     def is_dead(self):
-        return not any(map(Cell.is_alive, self.cells))
-
-    def index
+        return not any(map(Cell.is_alive, self))
 
 
 class Cell:
-    def __init__(self):
+    def __init__(self, i):
         self.state = CellState.Dead
+        self.index = i
 
-    def lightning(self):
+    def catch_fire(self):
         if self.is_alive():
             self.state = CellState.OnFire
 
@@ -38,5 +55,3 @@ class CellState(Enum):
     Dead = 0
     Alive = 1
     OnFire = 2
-
-
