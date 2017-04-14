983fa9c2e8d77ee674f8ccf70856f46b4967dc68
Alex Johnson <alex_johnson3@yahoo.com>
2017-04-14 16:03:16 -0600

    Plots 3 and 4

--- generate_plots.py
+++ generate_plots.py
@@ -5,16 +5,18 @@ import numpy as np
 
 
 def read_file(file_name):
-    return np.genfromtxt('data/' + file_name, delimiter=',', usecols=list(range(0,40)))
+    return np.genfromtxt('data/' + file_name, delimiter=',')
 
 
 def read_range(file_name, interval):
     return np.stack([read_file(file_name + str(i)) for i in interval])
 
 
-def from_group(fit_func, base='single_ga', average=True):
-    data = read_range('{base}/{0}/{1}_{0}_fitness_'.format(fit_func, 'single' if base == 'single_ga' else 'two', base=base), range(1, 8))
-    return np.average(data, 0) if average else data
+def from_group(fit_func, base='single_ga', num='single', output='fitness', average=True):
+    data = read_range(
+        '{base}/{f}/{num}_{f}_{out}_'.format(f=fit_func, base=base, num=num, out=output),
+        range(1, 8))
+    return np.average(data, 0)[:, 0:-1] if average else data
 
 
 def save(file_name):
@@ -54,22 +56,31 @@ def plot1():
 @new_fig_save(fn="fig2a", title="Biomass based Fitness Convergence", xlabel="Iteration #", ylabel="Fitness")
 def plot2a():
     biomass = np.average(from_group('biomass'), axis=1)
-    plt.plot(biomass)
+    plt.plot(biomass[0:50])
 
 
-@new_fig_save(fn="fig2b", title="Longevity based Fitness Convergence", xlabel="Iteration #", ylabel="Fitness")
-def plot2b():
-    plt.plot()
-
-
-@new_fig_save(fn="fig3a", title="Two Species Evolved Growth Rates", xlabel="Biomass", ylabel="Fitness")
+@new_fig_save(fn="fig3a", title="Two Species Evolved Growth Rates", xlabel="Iteration #", ylabel="Fitness")
 def plot3a():
-    p_vals = from_group('biomass', base='two_ga', average=False)
+    biomass = np.average(from_group('biomass', base='two_ga', num='two'), axis=1)
 
-    plt.plot()
+    plt.plot(biomass[0:50])
 
+
+@new_fig_save(fn="fig3b", title="Two Species Growth Rates", xlabel="Trial #", ylabel="Growth Rate (/ 100)")
+def plot3b():
+    biomass = np.average(from_group('biomass', base='firefighter_biomass', num='ff'), axis=1)
+
+    plt.plot(biomass[0:50])
+
+
+@new_fig_save(fn="fig4a", title="Biomass vs Firefighters", xlabel="Number of Firefighters", ylabel="Fitness")
+def plot4a():
+    biomass = np.average(np.average(read_range('firefighter_biomass/ff_biomass_fitness_', range(1, 11)), 0)[:,0:-1], axis=1)
+
+    plt.plot(np.arange(0, len(biomass)) * 50, biomass)
 
 plot1()
 plot2a()
-#plot2b()
 plot3a()
+#plot3b()
+plot4a()
