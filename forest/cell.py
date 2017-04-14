d8900304860a688867ad4c2c74f81167d1eb6ac4
Alex Johnson <alex_johnson3@yahoo.com>
2017-04-13 19:50:25 -0600

    Implement forest simulation in python using numpy. Add gui to visualize the evolution.

--- forest/cell.py
+++ forest/cell.py
@@ -1,61 +1,54 @@
-from enum import Enum
+from itertools import product
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+from matplotlib.animation import FuncAnimation
+
+DEAD = np.uint8(0)
+ALIVE = np.uint8(1)
+ON_FIRE = np.uint8(2)
 
 
-class Forest(list):
-    def __init__(self, rows=50, cols=50, spawn_rate_one=30, spawn_rate_two=0):
-        super().__init__(Cell(i) for i in range(rows * cols))
+class Forest:
+    def __init__(self, rows=250, cols=250, p1=0.3, p2=0, display=False):
+        self.cells = np.zeros((rows, cols), dtype=np.uint8)
         self.rows = rows
         self.cols = cols
-		self.spawn_rate_one
-		self.spawn_rate_two
+        self.p = p1 + p2
+        self.deltas = np.array([x + y * self.rows for (x, y) in product(range(-1, 2), range(-1, 2))])
 
-    def index_2d(self, i):
-        return i // self.cols, i % self.cols
+        if not display: return
 
-    def index_linear(self, r, c):
-        return self[r * self.cols + c]
+        # This code follows the examples at http://scipython.com/blog/the-forest-fire-model/
+        colors_list = ['brown', 'green', 'red', 'orange']
+        cmap = colors.ListedColormap(colors_list)
+        bounds = [0, 1, 2, 3]
+        norm = colors.BoundaryNorm(bounds, cmap.N)
 
-    def neighbors(self, cell):
-        r, c = self.index_2d(cell.index)
-        return [self.index_linear(r + dr, c + dc)
-                for dr in range(-1, 2, 1) for dc in range(-1, 2, 1)
-                if (dr != 0 or dc != 0) and
-                0 <= r + dr < self.rows and
-                0 <= c + dc < self.cols]
+        def step_and_redraw(*_):
+            self.step()
+            self.image.set_array(self.cells)
+            return self.image,
+
+        fig = plt.figure()
+        self.image = plt.imshow(self.cells, cmap=cmap, norm=norm, animated=True)
+        animation = FuncAnimation(fig, step_and_redraw, interval=1000, blit=True)
+        plt.show()
 
     def step(self):
-        # TODO: the filter was originally taking into account the new_cell.catch_fire() calls
-        for fire in list(filter(Cell.on_fire, self)):
-            for new_cell in self.neighbors(fire):
-                new_cell.catch_fire()
+        dead = self.cells == DEAD
+        self.cells[dead] = np.random.rand(np.sum(dead)) < self.p
+
+        flat = self.cells.flat
+        (x0,) = np.where(flat == ON_FIRE)
+        xs = np.repeat(x0, 9).reshape(-1, 9) + self.deltas
+        xs = xs[np.logical_and(xs >= 0, xs < self.rows * self.cols)]
+
+        flat[xs] = (flat[xs] & ALIVE) << 1
+
+        alive = self.cells == ALIVE
+        self.cells[alive] = ALIVE + (np.random.rand(np.sum(alive)) < 0.01)
 
     def is_dead(self):
-        return not any(map(Cell.is_alive, self))
-
-	def biomass(self):
-		return len(map(Cell.is_alive, self))/(rows*cols)
-
-class Cell:
-    def __init__(self, i):
-        self.state = CellState.Dead
-        self.index = i
-
-    def catch_fire(self):
-        if self.is_alive():
-            self.state = CellState.OnFire
-
-    def step(self):
-        if self.on_fire():
-            self.state = CellState.Dead
-
-    def is_alive(self):
-        return self.state != CellState.Dead
-
-    def on_fire(self):
-        return self.state == CellState.OnFire
-
-
-class CellState(Enum):
-    Dead = 0
-    Alive = 1
-    OnFire = 2
+        return np.sum(self.cells == ALIVE) == 0
--- test.py
+++ test.py
@@ -1,13 +1,8 @@
 from forest.cell import *
 
-forest = Forest(50, 50)
-for c in forest: c.state = CellState.Alive
-forest[0].state = CellState.OnFire
+forest = Forest(250, 250, display=True)
 
-print(forest.neighbors(forest[0]))
-
-while True:
-    num_fire = len(list(filter(Cell.on_fire, forest)))
-    print(num_fire)
+for _ in range(5000):
     forest.step()
-    input()
+
+print("Done")
